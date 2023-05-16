d7a8d0fe09ec3dc17cffa2fea90d9ec335b1a3c6
zendo <linzway@qq.com>
2023-05-16 16:57:15 +0800

    user optionals

--- nixos/user.nix
+++ nixos/user.nix
@@ -1,6 +1,8 @@
 {
   username,
   pkgs,
+  lib,
+  config,
   ...
 }: {
   users = {
@@ -15,36 +17,38 @@
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMLrQVhdLD9o1Iq17LKFNQ21PaHIAylizOFkvh74FUrz linzway@qq.com"
     ];
-    extraGroups = [
-      "wheel"
-      "audio"
-      "video"
-      "networkmanager"
-      "lxd"
-      "docker"
-      "libvirtd"
-      "qemu-libvirtd"
-      "vboxusers"
-      "adbusers"
-    ];
+    extraGroups =
+      [
+        "wheel"
+        "audio"
+        "video"
+        "networkmanager"
+      ]
+      ++ lib.optionals config.virtualisation.lxd.enable ["lxd"]
+      ++ lib.optionals config.virtualisation.docker.enable ["docker"]
+      ++ lib.optionals config.virtualisation.podman.enable ["podman"]
+      ++ lib.optionals config.virtualisation.libvirtd.enable ["libvirtd"]
+      # ++ lib.optionals config.virtualisation.virtualbox.host.enable ["vboxusers"]
+      ++ lib.optionals config.programs.adb.enable ["adbusers"];
   };
 
   users.users.guest = {
     isNormalUser = true;
     # passwd
     password = "guest";
-    extraGroups = [
-      "wheel"
-      "audio"
-      "video"
-      "networkmanager"
-      "lxd"
-      "docker"
-      "libvirtd"
-      "qemu-libvirtd"
-      "vboxusers"
-      "adbusers"
-    ];
+    extraGroups =
+      [
+        "wheel"
+        "audio"
+        "video"
+        "networkmanager"
+      ]
+      ++ lib.optionals config.virtualisation.lxd.enable ["lxd"]
+      ++ lib.optionals config.virtualisation.docker.enable ["docker"]
+      ++ lib.optionals config.virtualisation.podman.enable ["podman"]
+      ++ lib.optionals config.virtualisation.libvirtd.enable ["libvirtd"]
+      # ++ lib.optionals config.virtualisation.virtualbox.host.enable ["vboxusers"]
+      ++ lib.optionals config.programs.adb.enable ["adbusers"];
   };
 
   # security.sudo.wheelNeedsPassword = false;
