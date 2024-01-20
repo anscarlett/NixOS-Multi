d34042fd93195fc66802963d1164d7cc25fd96b2
zendo <linzway@qq.com>
2024-01-20 16:43:37 +0800

    sops fix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,5 +2,15 @@
 # https://blog.sekun.net/posts/manage-secrets-in-nixos/
 # https://lgug2z.com/articles/handling-secrets-in-nixos-an-overview/
 # https://www.youtube.com/watch?v=G5f6GC7SnhU
+
+/*
+mkdir -p ~/.config/sops/age
+
+age-keygen -o ~/.config/sops/age/keys.txt
+or
+ssh-to-age -private-key -i ~/.ssh/id_ed25519 > ~/.config/sops/age/keys.txt
+
+age-keygen -y ~/.config/sops/age/keys.txt
+*/
 {
 }
