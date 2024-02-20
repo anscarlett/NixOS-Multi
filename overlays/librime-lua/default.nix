528fa3ca1caabd617f03661d33f5131e315640d6
zendo <linzway@qq.com>
2024-02-20 10:21:03 +0800

    librime-lua update

--- overlays/librime-lua/default.nix
+++ overlays/librime-lua/default.nix
@@ -1,29 +1,32 @@
-{ lib
-, stdenv
-, fetchFromGitHub
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
 }:
 
-stdenv.mkDerivation (finalAttrs: {
-  pname = "librime-lua";
-  version = "2023-06-25";
+stdenv.mkDerivation (
+  finalAttrs: {
+    pname = "librime-lua";
+    version = "2024-02-06";
 
-  src = fetchFromGitHub {
-    owner = "hchunhui";
-    repo = "librime-lua";
-    rev = "c985eb399d63c491c6e03fa1651ddb59e485c04a";
-    hash = "sha256-tpyX0NxAlNZ4Qeml2BeM0NbPN5xDW/uH/f0eqjBQKIM=";
-  };
+    src = fetchFromGitHub {
+      owner = "hchunhui";
+      repo = "librime-lua";
+      rev = "7f3eca2ce659fc2401b8acb52bd2182b433e12b1";
+      hash = "sha256-n+KCu8JmFBGPyfBgeLiFqND3wmQs/4eOZjqTXuaW+hk=";
+    };
 
-  installPhase = ''
-    mkdir -p $out
-    cp -r ./* $out
-  '';
+    installPhase = ''
+      mkdir -p $out
+      cp -r ./* $out
+    '';
 
-  meta = {
-    description = "Extending RIME with Lua scripts";
-    homepage = "https://github.com/hchunhui/librime-lua";
-    license = lib.licenses.bsd3;
-    platforms = lib.platforms.all;
-    maintainers = with lib.maintainers; [ zendo ];
-  };
-})
+    meta = {
+      description = "Extending RIME with Lua scripts";
+      homepage = "https://github.com/hchunhui/librime-lua";
+      license = lib.licenses.bsd3;
+      platforms = lib.platforms.all;
+      maintainers = with lib.maintainers; [ zendo ];
+    };
+  }
+)
--- overlays/rime-ice/default.nix
+++ overlays/rime-ice/default.nix
@@ -1,38 +1,41 @@
-{ lib
-, stdenvNoCC
-, fetchFromGitHub
+{
+  lib,
+  stdenvNoCC,
+  fetchFromGitHub,
 }:
 
-stdenvNoCC.mkDerivation (finalAttrs: {
-  pname = "rime-ice";
-  version = "2023-10-22";
+stdenvNoCC.mkDerivation (
+  finalAttrs: {
+    pname = "rime-ice";
+    version = "2023-10-22";
 
-  src = fetchFromGitHub {
-    owner = "iDvel";
-    repo = "rime-ice";
-    rev = "e05b887f00ea80184248cc6d8d44c5646c9231d0";
-    hash = "sha256-Bfl66Ld8Uyk04u3fCgiPIvUsd1QrS/dC2gOvbsrq6sI=";
-  };
+    src = fetchFromGitHub {
+      owner = "iDvel";
+      repo = "rime-ice";
+      rev = "6d438fb8f4de5e54e0fb2e1daf0635d729277493";
+      hash = "sha256-bKwzulM6Xl3+Xr0Nk9jNKXKfbDJyPr8u90jHceCVwo8=";
+    };
 
-  installPhase = ''
-    mkdir -p $out/share/rime-data
+    installPhase = ''
+      mkdir -p $out/share/rime-data
 
-    install -Dm644 *.{schema,dict}.yaml   $out/share/rime-data
-    install -Dm644 *.{lua,gram}           $out/share/rime-data
-    install -Dm644 symbols*.yaml          $out/share/rime-data
-    install -Dm644 default.yaml           $out/share/rime-data
+      install -Dm644 *.{schema,dict}.yaml   $out/share/rime-data
+      install -Dm644 *.{lua,gram}           $out/share/rime-data
+      install -Dm644 symbols*.yaml          $out/share/rime-data
+      install -Dm644 default.yaml           $out/share/rime-data
 
-    mv lua       $out/share/rime-data
-    mv opencc    $out/share/rime-data
-    mv cn_dicts  $out/share/rime-data
-    mv en_dicts  $out/share/rime-data
-  '';
+      mv lua       $out/share/rime-data
+      mv opencc    $out/share/rime-data
+      mv cn_dicts  $out/share/rime-data
+      mv en_dicts  $out/share/rime-data
+    '';
 
-  meta = {
-    description = "雾凇拼音，功能齐全，词库体验良好，长期更新修订";
-    homepage = "https://github.com/iDvel/rime-ice";
-    license = lib.licenses.gpl3Plus;
-    platforms = lib.platforms.all;
-    maintainers = with lib.maintainers; [ zendo ];
-  };
-})
+    meta = {
+      description = "雾凇拼音，功能齐全，词库体验良好，长期更新修订";
+      homepage = "https://github.com/iDvel/rime-ice";
+      license = lib.licenses.gpl3Plus;
+      platforms = lib.platforms.all;
+      maintainers = with lib.maintainers; [ zendo ];
+    };
+  }
+)
